e208e592a0407092b55b7796d72d4cc8a978a1c5
Jacie Aaron <jacie.aaron@mavs.uta.edu>
2022-05-01 15:22:51 -0500

    Made text input

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char** argv) 
 {
-	std::cout << "Hello, World!" << std::endl;
+	std::cout << "Please enter an expression for evaluation:" << std::endl;
+	std::string expression;
+	std::getline(std::cin, expression);
+
+	std::cout << "You entered \"" << expression << "\"." << std::endl; 
 	return 0;
 }
