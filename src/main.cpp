5d0fdb02a57ea574c6bf5e3bf0528963708e2ba4
Jacie Aaron <jacie.aaron@mavs.uta.edu>
2022-05-01 16:06:46 -0500

    Added repeadted calculations or exit function

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,28 @@
 
 int main(int argc, char** argv) 
 {
-	std::cout << "Please enter an expression for evaluation:" << std::endl;
-	std::string expression;
-	std::getline(std::cin, expression);
+	std::string exit = "exit";
+	bool calculating = true;
+	while (calculating)
+	{
+		std::cout << "Please enter an expression for evaluation or type \"" << exit << "\" to quit: " << std::endl;
+		std::string expression;
+		std::getline(std::cin, expression);
+		if (expression == exit) calculating = false;
+		else
+		{
+			const char op = '+';
+			const auto position = expression.find(op);
 
-	const char op = '+';
-	const auto position = expression.find(op);
+			const auto lhs = expression.substr(0, position);
+			const auto rhs = expression.substr(position + 1, expression.size() - position - 1);
 
-	const auto lhs = expression.substr(0, position);
-	const auto rhs = expression.substr(position + 1, expression.size() - position - 1);
+			auto lhs_value = std::stoi(lhs);
+			auto rhs_value = std::stoi(rhs);
+			auto result = lhs_value + rhs_value;
 
-	auto lhs_value = std::stoi(lhs);
-	auto rhs_value = std::stoi(rhs);
-	auto result = lhs_value + rhs_value;
-
-	std::cout << "The answer is: " << result << "." << std::endl; 
+			std::cout << "The answer is: " << result << "." << std::endl << std::endl;
+		}
+	}
 	return 0;
 }
